eec92ab8fc5e45229d28120ae23c60cb3188cee5
Daniel <danielmelaku1177@gmail.com>
2025-01-24 16:59:22 +0300

    added dark and light mode

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//step 2 write the api and save in variable
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
@@ -5,13 +6,25 @@ const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+const colorChangeBtn = document.getElementById('colorChangeBtn');
 
-// Get initial movies
+const body = document.body;
+const overviews = document.querySelectorAll('.overview');
+
+//how the seach functionality works geneally
+//get the search value and save in variable
+//
+
 getMovies(API_URL)
+
+
+
 
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
+    console.log(data)
+    
 
     showMovies(data.results)
 }
@@ -39,7 +52,7 @@ function showMovies(movies) {
         main.appendChild(movieEl)
     })
 }
-
+//rate functionality which changes the color
 function getClassByRate(vote) {
     if(vote >= 8) {
         return 'green'
@@ -49,7 +62,7 @@ function getClassByRate(vote) {
         return 'red'
     }
 }
-
+//seach functioality
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -63,3 +76,34 @@ form.addEventListener('submit', (e) => {
         window.location.reload()
     }
 })
+
+
+colorChangeBtn.addEventListener('click', () => {
+   
+
+    // Toggle colors
+    if (body.style.backgroundColor === 'black') {
+        colorChangeBtn.innerHTML='light'
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+        main.classList.remove('dark-hover');
+        
+        // Change overview color for light mode
+        overviews.forEach(overview => {
+            overview.style.backgroundColor ='black'; // Light background for overview
+            overview.style.color = 'white'; // Dark text for overview
+        });
+    } else {
+        colorChangeBtn.innerHTML='dark'
+
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+        main.classList.add('dark-hover');
+        
+        // Change overview color for dark mode
+        overviews.forEach(overview => {
+            overview.style.backgroundColor = 'white'; // Semi-transparent white for overview
+            overview.style.color = 'black'; // Light text for overview
+        });
+    }
+});
